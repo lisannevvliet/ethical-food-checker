d6c5bb30b22aa5b82dc464dc9b13ab6991781b3b
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-03-01 11:30:23 +0100

    Placeholder afbeelding toegevoegd

--- js/modules/render.js
+++ js/modules/render.js
@@ -1,19 +1,23 @@
-import { $ } from './$.js';
 import { emojis } from './emojis.js';
+import { $ } from './$.js';
 
 export function render(data) {
     for (let i in data.products) {
         if (data.products[i].product_name != '') {
-            // Empty list of ingredients.
-            let ingredients = ''
+            // Empty string for the image.
+            let image = ''
 
-            // Fill the list of ingredients if there are any.
-            if (data.products[i].ingredients_text) { ingredients = `Ingredients: ${data.products[i].ingredients_text}.<br>` }
+            // Add the image if there is any.
+            if (data.products[i].image_small_url) {
+                image = `<div class='image'><img src='${data.products[i].image_small_url}' alt='${data.products[i].product_name}'></div>`
+            } else {
+                image = `<div class='image'><img src='img/placeholder-image.png' alt='${data.products[i].product_name}'></div>`
+            }
             
             // Add the product name, ingredients and image to the page.
             $('ul').insertAdjacentHTML('beforeend',`<li><button class='product'>
-            <img src='${data.products[i].image_small_url}' alt='${data.products[i].product_name}' class='image'>
-            <span class='span'><strong>${data.products[i].product_name}</strong> ${emojis(data.products[i].ingredients_analysis_tags)}</span><br>
+            ${image}
+            <span class='span'><strong>${data.products[i].product_name}</strong><br> ${emojis(data.products[i].ingredients_analysis_tags)}</span><br>
             </button><br><br></li>`)
         }
     }
