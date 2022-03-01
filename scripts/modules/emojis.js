8051d9337c300adb20e028db076217509610c19e
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-03-01 13:41:04 +0100

    Tooltip toegevoegd

--- scripts/modules/emojis.js
+++ scripts/modules/emojis.js
@@ -5,11 +5,11 @@ export function emojis(ingredients_analysis_tags) {
     // Check if there are any ingredients analysis tags.
     if (ingredients_analysis_tags) {
         // If the product is vegetarian, add an emoji.
-        if (ingredients_analysis_tags[2] == 'en:vegetarian') { emojis.push('🐄') }
+        if (ingredients_analysis_tags[2] == 'en:vegetarian') { emojis.push('<div class=\'tooltip\'>🐄<span class=\'tooltiptext\'>Vegetarian</span></div>') }
         // If the product is vegan, add an emoji.
-        if (ingredients_analysis_tags[1] == 'en:vegan') { emojis.push('🌱') }
+        if (ingredients_analysis_tags[1] == 'en:vegan') { emojis.push('<div class=\'tooltip\'>🌱<span class=\'tooltiptext\'>Vegan</span></div>') }
         // If the product is free from palm oil, add an emoji.
-        if (ingredients_analysis_tags[0] == 'en:palm-oil-free') { emojis.push('🌴') }
+        if (ingredients_analysis_tags[0] == 'en:palm-oil-free') { emojis.push('<div class=\'tooltip\'>🌴<span class=\'tooltiptext\'>Palm oil free</span></div>') }
     }
 
     // Return the corresponding emojis.
