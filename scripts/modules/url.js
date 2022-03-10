42c4dc8b27e82a99988247be82feb36482af354c
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-03-10 10:01:12 +0100

    Wereldwijde zoeken (i.p.v. enkel Nederland) geïmplementeerd

--- scripts/modules/url.js
+++ scripts/modules/url.js
@@ -3,8 +3,8 @@ import { barcode } from "./barcode.js"
 export function url(page, sort_by, hash) {
     // Save the correct URL based on if the search query is a barcode or not.
     let url =  barcode(hash)
-    ? `https://nl.openfoodfacts.org/cgi/search.pl?code=${hash}&search_simple=1&action=process&json=1&page=${page}`
-    : `https://nl.openfoodfacts.org/cgi/search.pl?search_terms=${hash}&search_simple=1&action=process&json=1&page=${page}`
+    ? `https://world.openfoodfacts.org/cgi/search.pl?code=${hash}&search_simple=1&action=process&json=1&page=${page}`
+    : `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${hash}&search_simple=1&action=process&json=1&page=${page}`
 
     // If there is a sort method, add this to the url.
     if (sort_by != "") { url += `&sort_by=${sort_by}`; console.log(url) }
