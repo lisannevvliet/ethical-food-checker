0bc53898b674b46ec15d13196323b293f67b19e9
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2023-12-05 21:38:14 +0100

    Fix EcoScore images

--- scripts/modules/ecoscore.js
+++ scripts/modules/ecoscore.js
@@ -5,19 +5,19 @@ export function ecoscore(ecoscore_grade) {
     // Fill the ecoscore variable with the correct image.
     switch (ecoscore_grade) {
         case "a":
-            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/icons/ecoscore-a.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
+            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/attributes/dist/ecoscore-a.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
             break;
         case "b":
-            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/icons/ecoscore-b.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
+            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/attributes/dist/ecoscore-b.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
             break;
         case "c":
-            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/icons/ecoscore-c.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
+            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/attributes/dist/ecoscore-c.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
             break;
         case "d":
-            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/icons/ecoscore-d.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
+            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/attributes/dist/ecoscore-d.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
             break;
         case "e":
-            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/icons/ecoscore-e.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
+            ecoscore = `<div id="tooltip"><img src="https://static.openfoodfacts.org/images/attributes/dist/ecoscore-e.svg" alt="Eco-Score: ${ecoscore_grade.toUpperCase()}" id="ecoscore"><span>Eco-Score</span></div>`
     }
 
     return ecoscore
